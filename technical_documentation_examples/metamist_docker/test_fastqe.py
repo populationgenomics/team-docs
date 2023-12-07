ec079647dfd5411630ecad6d44560fec3660f8fb
Michael Harper <michael.harper@populationgenomics.org.au>
2023-12-08 07:29:24 +1000

    changed test_fastqe to using Hail batch

--- technical_documentation_examples/metamist_docker/test_fastqe.py
+++ technical_documentation_examples/metamist_docker/test_fastqe.py
@@ -1,19 +1,53 @@
-import subprocess
+"""
+Jobs to run FastQE.
+"""
 
-# Run FastQC on the input FASTQ file
-fastqe_command = [
-    "fastqe",
-    "test_fq_r1.fq",
-    "test_fq_r2.fq",
-    # "--html",
-    # ">",
-    # "test_fastqe.html",
-]
-# output = subprocess.run(fastqe_command, stdout=subprocess.PIPE, text=True)
+from os.path import basename
 
-# Run the command and redirect output to a file
-with open("test_fastqe.html", "w") as output_file:
-    subprocess.run(fastqe_command, stdout=output_file, text=True)
+import hailtop.batch as hb
+from hailtop.batch.job import Job
 
-# # Print the output to the console
-# print(output.stdout)
+from cpg_utils import Path
+from cpg_utils.hail_batch import command
+from cpg_utils.hail_batch import image_path
+from cpg_workflows.filetypes import (
+    BamPath,
+    FastqPath,
+)
+from cpg_workflows.resources import STANDARD
+
+
+def fastqe(
+    b: hb.Batch,
+    output_html_path: Path,
+    output_zip_path: Path,
+    input_path1: BamPath | FastqPath,
+    input_path2: BamPath | FastqPath,
+    job_attrs: dict | None = None,
+) -> Job:
+    """
+    Adds FastQE jobs. If the input is a set of fqs, runs FastQE on each fq file.
+    """
+    j = b.new_job("FASTQE", (job_attrs or {}) | {"tool": "fastqe"})
+    j.image(image_path("fastqe"))
+    threads = STANDARD.set_resources(j, ncpu=16).get_nthreads()
+
+    cmd = ""
+    input_file1: str | hb.ResourceFile = b.read_input(str(input_path1))
+    input_file2: str | hb.ResourceFile = b.read_input(str(input_path2))
+    fname1 = basename(str(input_path1))
+    fname2 = basename(str(input_path2))
+
+    cmd += f"""\
+    mkdir -p $BATCH_TMPDIR/outdir
+    fastqe {input_file1} {input_file2} \\
+    --outdir $BATCH_TMPDIR/outdir
+    ls $BATCH_TMPDIR/outdir
+    ln $BATCH_TMPDIR/outdir/*_fastqe.html {j.out_html}
+    ln $BATCH_TMPDIR/outdir/*_fastqe.zip {j.out_zip}
+    unzip $BATCH_TMPDIR/outdir/*_fastqe.zip
+    """
+    j.command(command(cmd, monitor_space=True))
+    b.write_output(j.out_html, str(output_html_path))
+    b.write_output(j.out_zip, str(output_zip_path))
+    return j
