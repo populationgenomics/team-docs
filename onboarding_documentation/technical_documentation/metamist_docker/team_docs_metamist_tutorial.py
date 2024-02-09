c988d69e2ef6d4ccf8ff80072e7b0d5fd29f0bf6
Michael Harper <michael.harper@populationgenomics.org.au>
2024-02-09 14:49:54 +1100

    adding script to run entire batch

--- onboarding_documentation/technical_documentation/metamist_docker/team_docs_metamist_tutorial.py
+++ onboarding_documentation/technical_documentation/metamist_docker/team_docs_metamist_tutorial.py
@@ -37,15 +37,15 @@ def get_assays(project: str, sgids: list) -> list[str]:
 
     # Use the query template above to query the sequencing groups and assays
     query_response = query(SG_ASSAY_QUERY, {"project": project, "sgids": sgids})
-    for sg in query_response["sequencingGroups"]:
-        sg_id = sg["id"]
-        for assay in sg["assays"]:
-            assay_meta = assay["meta"]
-            reads = assay_meta.get("reads")
+    for sg in query_response['sequencingGroups']:
+        sg_id = sg['id']
+        for assay in sg['assays']:
+            assay_meta = assay['meta']
+            reads = assay_meta.get('reads')
             if reads is None:
                 continue
             else:
-                read_locations = [read["location"] for read in reads]
+                read_locations = [read['location'] for read in reads]
             sg_assay_map[sg_id] = read_locations
 
     return sg_assay_map
@@ -61,22 +61,23 @@ def create_analysis_entry(
     from metamist.model.analysis import Analysis
     from metamist.model.analysis_status import AnalysisStatus
 
-    project = get_config()["workflow"]["dataset"]
-    if get_config()["workflow"]["access_level"] == "test" and "test" not in project:
-        project = f"{project}-test"
-    output_prefix = get_config()["workflow"]["output_prefix"]
+    project = get_config()['workflow']['dataset']
+    if get_config()['workflow']['access_level'] == 'test' and 'test' not in project:
+        project = f'{project}-test'
+    output_prefix = get_config()['workflow']['output_prefix']
     output_path = os.path.join(
-        get_config()["storage"]["default"]["web"], output_prefix, sgid, ".html"
+        get_config()['storage']['default']['web'], output_prefix, f'{sgid}.html'
     )
     display_url = os.path.join(
-        get_config()["storage"]["default"]["web_url"], output_prefix, f"{sgid}.html"
+        get_config()['storage']['default']['web_url'], output_prefix, f'{sgid}.html'
     )
     AnalysisApi().create_analysis(
-        project=f"{project}",
+        project=f'{project}',
         analysis=Analysis(
-            type="web",
-            status=AnalysisStatus("completed"),
-            meta={"display_url": display_url},
+            type='web',
+            status=AnalysisStatus('completed'),
+            meta={'display_url': display_url},
+            sequencing_group_ids=[sgid],
             output=output_path,
             active=active,
         ),
@@ -93,19 +94,19 @@ def main(project: str, sgids: list[str]):
     # endregion
 
     # region: Batch time
-    b = get_batch("Schedule some worker tasks")
+    b = get_batch('Schedule some worker tasks')
     for sg, files in file_dict.items():
         # check that we have 2 files (assuming FQ, rather than BAM)
         assert len(files) == 2
 
         # Create a job for each sample
-        j = b.new_job("FastQE", {"tool": "fastqe"})
+        j = b.new_job('FastQE', {'tool': 'fastqe'})
 
         # Set the docker image to use in this job
         # this pulls the image path from the portion of the config
         # populated by the images repository
-        j.image(image_path("fastqe"))
-        j.storage("40Gi")
+        j.image(image_path('fastqe'))
+        j.storage('2Gi')
 
         # read data into the batch tmp resource location
         file_1 = b.read_input(files[0])
@@ -114,15 +115,15 @@ def main(project: str, sgids: list[str]):
         # Set the command to run
         # batch.read_input will create a new path like /io/batch/75264c/CPGAAAA_1.fq.gz
         # accessible from inside the job container, and unique to this batch/job
-        cmd = f"fastqe {file_1} {file_2} --html > {j.out_fastqe}"
+        cmd = f'fastqe {file_1} {file_2} --html > {j.out_fastqe}'
         j.command(
             # f'echo "Hello world, I am a job for {sg}!, using {file_1} and {file_2}"'
             # f'I\'m also creating an output file at {j.output}'
             # f'echo "Some outputs" > {j.output}'
             command(cmd)
         )
-        j2 = b.new_python_job(f"Register analysis output for {sg}")
-        j2.image(get_config()["workflow"]["driver_image"])
+        j2 = b.new_python_job(f'Register analysis output for {sg}')
+        j2.image(get_config()['workflow']['driver_image'])
         j2.call(create_analysis_entry, sg)
         j2.depends_on(j)
         # read the output out into GCP
@@ -134,22 +135,22 @@ def main(project: str, sgids: list[str]):
         # output_path('this_file.txt')
         # -> gs://cpg-my-dataset-test/my_output/this_file.txt
         b.write_output(
-            j.out_fastqe, output_path(f"{sg}.html", category="web")
+            j.out_fastqe, output_path(f'{sg}.html', category='web')
         )  # category='web' writes it to the web bucket
     b.run(wait=False)
 
     # endregion
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # optional direct input of samples
-    parser = ArgumentParser(description="Hail Batch FastQE")
-    parser.add_argument("--project", help="Project name", required=True)
+    parser = ArgumentParser(description='Hail Batch FastQE')
+    parser.add_argument('--project', help='Project name', required=True)
     parser.add_argument(
-        "--sgids", nargs="+", help="Sequencing group IDs", required=True
+        '--sgids', nargs='+', help='Sequencing group IDs', required=True
     )
     args, fails = parser.parse_known_args()
 
     if fails:
-        raise ValueError(f"Failed to parse arguments: {fails}")
+        raise ValueError(f'Failed to parse arguments: {fails}')
     main(project=args.project, sgids=args.sgids)
